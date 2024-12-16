a1ae0a359cff80b0b958aeb2652db5872b486904
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-12-16 16:36:54 +0100

    added methods in Author and Book

--- tp/Author.cs
+++ tp/Author.cs
@@ -19,7 +19,7 @@ class Author()
         }
     }
 
-    public void getInfos(){
-
+    public void getFullName(){
+        Console.WriteLine($"{FirstName} ");
     }
 }
--- tp/Book.cs
+++ tp/Book.cs
@@ -11,9 +11,21 @@ class Book()
     }
 
     private DateTime publication_date;
+    DateTime PublicationDate {
+        get{
+            return publication_date;
+        }
+        set {
+            publication_date = value;
+        }
+    }
      
 
     Author author;
+
+    public void getInfos(){
+        Console.WriteLine($"title: {Title}, publication date: {PublicationDate}, author: {author.getFullName}.");
+    }
 
 
 }
