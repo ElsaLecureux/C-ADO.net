dd4b6dd9718732dc509bc0eaff4bf4506623e7f8
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-12-17 16:27:49 +0100

    exo3

---
+++ ADO.NET/exo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString= "Server=localhost;Database=MyDatabase;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=false;";
+
+        string query="CREATE TABLE Car (id INT PRIMARY KEY IDENTITY(1,1), brand VARCHAR(50), model VARCHAR(50), yearBuild INT, price DECIMAL(10, 2));";
+        string query1= @"INSERT INTO Car(brand, model, yearBuild, price)
+                            VALUES('Volvo', 'EX40', 2023, 49500),
+                            ('Volvo', 'XC90', 2025, 57400),
+                            ('Renault', 'Clio', 2019, 17840),
+                            ('Renault', 'Espace', 1995, 20899),
+                            ('Renault', 'Avantime', 2002, 45000)";
+        string query2="UPDATE Car SET price='19500', model= 'Megane' WHERE id = 5";
+        string query3="UPDATE Car SET price='17890' WHERE id = 3";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query3, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table created and updated successfully");
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
