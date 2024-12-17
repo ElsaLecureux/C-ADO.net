f2a5d9087729c5eb037a7de011733147d47f18f3
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-12-17 11:51:08 +0100

    added options 3, 4, 5

--- tp/Program.cs
+++ tp/Program.cs
@@ -4,18 +4,24 @@ using System.Formats.Asn1;
 using System.Security.Cryptography.X509Certificates;
 class Program
 {
-    public static List<Book> Library = [];
+    public static List<Book> Library = new List<Book>();
+    public static bool running = true;
     static void Main(string[] args)
     {
-        Options();
+        while(running){
+            Options();
+        }
     }
  
     public static void Options()
     {       
-             Console.WriteLine("Welcome into your Library!");
-            Console.WriteLine("Choose an option: ");
-    Console.WriteLine("1. Add a new Book to the Library");
-    Console.WriteLine("2. Display your books entered in the Library");
+        Console.WriteLine("Welcome into your Library!");
+        Console.WriteLine("Choose an option: ");
+        Console.WriteLine("1. Add a new Book to the Library");
+        Console.WriteLine("2. Display all books in the Library");
+        Console.WriteLine("3. Display a book: ");
+        Console.WriteLine("4. Delete a book: ");
+        Console.WriteLine("5. Exit Library");
     string option = Console.ReadLine()!;
         switch(option)
         {
@@ -23,11 +29,20 @@ class Program
                 AddMoreBook();            
                 break;
             case "2":
-                DisplayBooks();
+                DisplayAllBooks();
+                break;
+            case "3":
+                DisplayBook();
+                break;
+            case "4":
+                DeleteBook();
+                break;
+            case "5":
+                ConfirmExit();
                 break;
             default:
-                Console.WriteLine("Not an option");
-                break;
+                Console.WriteLine("Invalid option. Please try again.");
+                break;       
         }
     }
 
@@ -62,26 +77,76 @@ class Program
         }
         catch(FormatException exp)
         {
-            Console.WriteLine("wrong format:", exp.Message);
-            Console.WriteLine("Do you want to try to add the book again?");
-            string answer = Console.ReadLine()!;
-            if(answer == "yes" || answer == "y")
-                {
-                    AddMoreBook();
-                }
-                else{
-                    Options();
-                }    
+            Console.WriteLine($"{exp.Message}, Invalid field, please try again.");
         }
     }
 
-    public static void DisplayBooks()
+    public static void DisplayAllBooks()
     {
+        Console.WriteLine("Books in the library: ");
         foreach (Book book in Library)
         {
             book.getInfos();
         }
+         Console.WriteLine("Back to option: yes or no");
+            string answer = Console.ReadLine()!;
+            if(answer == "yes" || answer == "y")
+            {
+                Options();
+            }
+            else{
+                Console.WriteLine("Exit Library: yes or no");
+                string exitAnswer = Console.ReadLine()!;
+                if(answer == "yes" || answer == "y")
+                {
+                    running = false;
+                }
+            }    
+    }
 
+    public static void DisplayBook()
+    {
+        Console.WriteLine("Enter the exact title of the book you are looking for: ");
+        try
+        {
+            string title = Console.ReadLine()!;
+            Book book = Library.Find(book => book.Title == title)!;
+            book.getInfos();       
+        }
+        catch(Exception exp)
+        {
+            Console.WriteLine($"{exp.Message}, No book found in the library for this title: {Console.ReadLine()!}");
+            
+        }
+    }
+
+    public static void DeleteBook()
+    {
+        Console.WriteLine("Enter the exact title of the book you want to delete: ");
+        try
+        {
+            string title = Console.ReadLine()!;
+            Book book = Library.Find(book => book.Title == title)!;
+            Library.Remove(book); 
+            Console.WriteLine("This book was deleted from the library.");      
+        }
+        catch(Exception exp)
+        {
+            Console.WriteLine($"{exp.Message}, No book found in the library for this title: {Console.ReadLine()!}");
+        }
+    }
+
+    public static void ConfirmExit()
+    {
+        Console.WriteLine("Confirm exist Library: yes or no");
+        string answer = Console.ReadLine()!;
+        if (answer == "yes" || answer == "Yes" || answer == "y")
+        {
+            running = false;
+        }
+        else {
+            Options();
+        }
     }
 
 }
